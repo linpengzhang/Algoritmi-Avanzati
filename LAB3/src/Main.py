d87dee6aae6c1e5c34bcde8c9ab933b4a7679cf2
Linpeng <zhanglinpeng1998@gmail.com>
2019-04-07 21:56:42 +0200

    held_karp, geo

--- LAB3/src/Main.py
+++ LAB3/src/Main.py
@@ -1,10 +1,12 @@
 from Graph_FromFile import GraphFromFile
-
+from held_karp import *
 
 print("Script start")
 
 # Read file and create main graph
 print("Reading graph from file(s)...")
-real_graph = GraphFromFile("./inputFiles/kroD100.tsp")
+real_graph = GraphFromFile("./inputFiles/burma14.tsp")
+print("ciao")
+print(hk_tsp(real_graph))
 print("Graph created.")
 
