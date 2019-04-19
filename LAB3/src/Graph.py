9049b2a01e84a4c851fec0959eb7e201a7b069a9
mek11 <martino.echerle@gmail.com>
2019-04-19 14:04:26 +0200

    miglioramenti codice

--- LAB3/src/Graph.py
+++ LAB3/src/Graph.py
@@ -1,14 +1,24 @@
 from collections import defaultdict
 
 class Graph:
+
     def __init__(self):
         self.graph = [[]]
+
     def number_of_nodes(self):
         """
-        :return: Return the number of nodes (stations) in the graph
+        :return: Return the number of nodes in the graph
         """
         return len(self.graph)
+
     def set_of_nodes(self):
+        """
+        :return: Return a set with the nodes in the graph
+        """
         return frozenset(range(self.number_of_nodes))
+
     def list_of_nodes(self):
+        """
+        :return: Return a list with the nodes in the graph
+        """
         return list(range(self.number_of_nodes))
--- LAB3/src/cheapest_insertion.py
+++ LAB3/src/cheapest_insertion.py
@@ -1,26 +1,39 @@
 from Graph_FromFile import GraphFromFile
+import numpy as np
+from functools import reduce
+
 def cheapest_insertion(G):
+    # C memorizza il circuito
+    # parto dal circuito parziale composto dal solo nodo 0
     C = [0]
-    #min_w = min(x for x in G.graph[0] if x>1)
-    min_w = min(G.graph[0])
-    min_index = G.graph[0].index(min_w)
-    C.append(min_index)
-    C.append(0)
-    result = 2*min_w
+    # V contiene i vertici non ancora presenti nel circuito parziale
     V = {i for i in range(1, G.number_of_nodes())}
-    V.remove(min_index)
+    # trovo un vertice j che minimizza la distanza w(0,j)
+    j = np.argmin(G.graph[0][1:])+1
+    V.remove(j)
+    # costruisco il circuito parziale (0,j,0)
+    C.append(j)
+    C.append(0) 
+    #NOTA: non posso calcolare subito il risultato perche' inserendo nodi nel mezzo del cammino certi archi non li considero piu'!
+    # itero per tutti gli altri nodi rimanenti
     for _ in range(2, G.number_of_nodes()):
+        # trovo un vertice k non presente nel circuito parziale e un arco {i,j}
+        # del circuito parziale che minimizzano la distanza w(i,k)+w(k,j)-w(i,j)
         min_w = float("inf")
         for k in V:
-            for i in range(len(C)-1):
-                j=i+1
-                actual_w = G.graph[C[i]][k] + G.graph[k][C[j]] - G.graph[C[i]][C[j]]
-                if actual_w<min_w:
+            for ii in range(len(C)-1):
+                i = C[ii]
+                j = C[ii+1]
+                actual_w = G.graph[i][k] + G.graph[k][j] - G.graph[i][j]
+                if actual_w < min_w:
                     min_w = actual_w
-                    ii = C[i]
-                    jj = C[j]
-                    kk = k
-        result = result + min_w
-        V.remove(kk)
-        C.insert(C.index(jj),kk)
+                    idx_j = ii+1 # indice al quale inserire k in C
+                    found_k = k # vertice k da aggiungere
+        # inserisco k tra i e j
+        C.insert(idx_j,found_k)
+        V.remove(found_k)
+    # calcola il peso del circuito trovato
+    result = 0
+    for i in range(len(C)-1):
+        result += G.graph[C[i]][C[i+1]]
     return result
