41613aecde64f8d3e8559cc94629a073629d5890
mek11 <martino.echerle@gmail.com>
2019-04-26 14:48:58 +0200

    spostate euristiche costruttive tsp in file separato

--- LAB3/src/Main.py
+++ LAB3/src/Main.py
@@ -1,14 +1,13 @@
 from Graph_FromFile import GraphFromFile
-from cheapest_insertion import cheapest_insertion
 from held_karp import hk_tsp
-from random_insertion import random_insertion
+import tsp_constructive_heuristics
 
 files_names = ['burma14.tsp', 'ulysses22.tsp', 'eil51.tsp', 'kroD100.tsp', 'gr229.tsp', 'd493.tsp', 'dsj1000.tsp']
 files_descriptions = ['Birmania (Myanmar)', 'Mediterraneo', 'Sintetico', 'Random', 'Asia/Australia', 'Foratura di circuiti stampati', 'Random.tsp']
 optimal_solutions = [3323, 7013, 426,21294, 134602, 35002, 18659688]	 
 
 # index from 0 to 6
-index = 6
+index = 4
 
 print("Script start")
 
@@ -26,11 +25,14 @@ print("The optimal solution is: ", optimal_solutions[index])
 #print(hk_tsp(real_graph))
 
 # Run constructive heuristic algorithm
-#print("Running: Cheapest Insertion")
-#print("Solution:", cheapest_insertion(real_graph))
+print("Running: Nearest Neighbor")
+print("Solution:", tsp_constructive_heuristics.nearest_neighbor(real_graph))
+
+print("Running: Cheapest Insertion")
+print("Solution:", tsp_constructive_heuristics.cheapest_insertion(real_graph))
 
 print("Running: Random Insertion")
-print("Solution:", random_insertion(real_graph))
+print("Solution:", tsp_constructive_heuristics.random_insertion(real_graph))
 
 
 
