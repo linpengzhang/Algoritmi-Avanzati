db5dbf54837d77b26cc37fd737ac6fa5ec3783e7
Linpeng <zhanglinpeng1998@gmail.com>
2019-04-27 10:18:48 +0200

    tsp che forse va

--- LAB3/src/Main.py
+++ LAB3/src/Main.py
@@ -2,6 +2,7 @@ from Graph_FromFile import GraphFromFile
 from cheapest_insertion import cheapest_insertion
 from held_karp import hk_tsp
 from random_insertion import random_insertion
+from tsp import *
 
 files_names = ['burma14.tsp', 'ulysses22.tsp', 'eil51.tsp', 'kroD100.tsp', 'gr229.tsp', 'd493.tsp', 'dsj1000.tsp']
 files_descriptions = ['Birmania (Myanmar)', 'Mediterraneo', 'Sintetico', 'Random', 'Asia/Australia', 'Foratura di circuiti stampati', 'Random.tsp']
@@ -29,8 +30,10 @@ print("The optimal solution is: ", optimal_solutions[index])
 #print("Running: Cheapest Insertion")
 #print("Solution:", cheapest_insertion(real_graph))
 
+"""
 print("Running: Random Insertion")
 print("Solution:", random_insertion(real_graph))
-
+"""
+print("Solution:", random_insertion(real_graph))
 
 
--- LAB3/src/tsp.py
+++ LAB3/src/tsp.py
@@ -1,18 +1,47 @@
 from Graph_FromFile import GraphFromFile
-def prim_mst(G, w,r):
+from collections import defaultdict
+def prim_mst_completo(G):
     """
-    G: grafo connesso non orientato pesato
-    w: peso
-    r: vertice in V
+    G: grafo completo non orientato pesato
+    G.graph: matrice delle adiacenze di G
+    :return: albero di copertura minimo
     """
-    for v in G.graph:
+    key = defaultdict()
+    p = defaultdict()
+    for v in range(1,G.number_of_nodes()):
         key[v] = float("inf")
         p[v] = None
-    key[r] = 0
-    Q = G.list_of_nodes()
-    while len(Q)>0:
-        min_key = min(key)
-        for v in G.graph[key.index(min_key)]:
-            if v in Q and w[u][v] < key[v]:
-                key[v] = w[u][v]
+    key[0] = 0
+    while len(key)>0:
+        min_attuale = float("inf")
+        for i in key.keys():
+            if key[i]<min_attuale:
+                u = i
+        key.pop(u)
+        for v in key.keys():
+            if G.graph[u][v] < key[v]:
+                key[v] = G.graph[u][v]
                 p[v] = u
+    Adj = defaultdict(set)
+    for i in range(1,G.number_of_nodes()):
+        Adj[i].add(p[i])
+        Adj[p[i]].add(i)
+    return Adj
+
+def dfs_visited_tree(Adj, visited, u, path):
+    visited[u] = True
+    path.append(u)
+    for v in Adj[u]:
+        if not visited[v]:
+            dfs_visited_tree(Adj, visited, v, path)
+    return path
+
+def tsp(G: GraphFromFile):
+    Adj = prim_mst_completo(G)
+    visited = [False for i in range(G.number_of_nodes())]
+    path = dfs_visited_tree(Adj, visited, 0, [])
+    path.append(0)
+    somma = 0
+    for i in range(len(path)-1):
+        somma = somma + G.graph[path[i]][path[i+1]]
+    return somma
