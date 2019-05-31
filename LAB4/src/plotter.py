bd8c2a6dff55b364001eac360f87a5f23888ebcd
mek11 <martino.echerle@gmail.com>
2019-05-31 17:23:11 +0200

    cambiati i colori più scuri

--- LAB4/src/plotter.py
+++ LAB4/src/plotter.py
@@ -6,7 +6,7 @@ from itertools import cycle
 def draw_clustering(clustering):
     img = mpimg.imread('inputFiles/USA_Counties.png')
 
-    colors = cycle(['#800000', '#9A6324', '#469990', '#000075', '#000000',
+    colors = cycle(['#800000', '#9A6324', '#469990', '#0000ad', '#c0be42',
                     '#e6194B', '#f58231', '#ffe119', '#3cb44b', '#42d4f4',
                     '#4363d8', '#911eb4', '#f032e6', '#8e8e8e', '#fabebe',
                     '#e6beff'])
