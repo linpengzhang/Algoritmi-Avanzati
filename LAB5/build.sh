87103b4ba7e3aeae2d7fcbb40dd6b234e6de46c9
Michele <mikelerogeratro@yahoo.it>
2019-06-12 18:21:03 +0200

    Util scripts

---
+++ LAB5/build.sh
@@ -0,0 +1,2 @@
+cd src
+javac -cp ".;xchart-3.5.4.jar" *.java
---
+++ LAB5/run.bat
@@ -0,0 +1,7 @@
+@echo off
+cd src
+@echo on
+java -cp ".;xchart-3.5.4.jar" Main
+@echo off
+echo -------------------------------
+pause
